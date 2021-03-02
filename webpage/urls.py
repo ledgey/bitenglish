e58f78fec26c9f3e3dce9b37ddaadf9c81931f1d
ledgey <craig.ledgerwood81@gmail.com>
2021-03-02 00:08:35 +0000

    added extra images, changed bullet points, added base for about page

--- webpage/urls.py
+++ webpage/urls.py
@@ -3,5 +3,6 @@ from django.urls import include, path
 from . import views
 
 urlpatterns = [
-    path('', views.Home.as_view(), name='home')
+    path('', views.Home.as_view(), name='home'),
+    path('about', views.About.as_view(), name='about')
 ]
--- webpage/views.py
+++ webpage/views.py
@@ -5,3 +5,8 @@ class Home(View):
     def get(self, request, *args, **kwargs):
         return render(request, template_name='home.html')
 
+
+class About(View):
+    def get(self, request, *args, **kwargs):
+        return render(request, template_name='about.html')
+
